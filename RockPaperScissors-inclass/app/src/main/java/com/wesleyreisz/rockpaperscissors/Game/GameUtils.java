285f2f23ee0a3cbe4fc2ebc7c7aa66545c0a7677
Colin Phelps <cphelps56@gmail.com>
2016-09-12 16:06:13 -0400

    finished TODOs for Homework assignment 2

--- RockPaperScissors-inclass/app/src/main/java/com/wesleyreisz/rockpaperscissors/Game/GameUtils.java
+++ RockPaperScissors-inclass/app/src/main/java/com/wesleyreisz/rockpaperscissors/Game/GameUtils.java
@@ -3,6 +3,7 @@ package com.wesleyreisz.rockpaperscissors.Game;
 import android.graphics.Color;
 import com.wesleyreisz.rockpaperscissors.R;
 
+import java.util.Objects;
 import java.util.Random;
 
 /**
@@ -29,8 +30,59 @@ public class GameUtils {
         }
 
     }
+    public interface GameItem{
+        String eval(int computerChoice);
+    }
+    static class Paper implements GameItem{
 
-    public static String evaluateWinner(Integer playerSelectedChoice, Integer computerSelectedChoice) {
+        @Override
+        public String eval(int computerChoice) {
+            {
+                if (Objects.equals(computerChoice, R.id.btnPaper)) {
+                    return TIES;
+                } else if (Objects.equals(computerChoice, R.id.btnRock)) {
+                    return BEATS;
+                } else {
+                    return LOSES_TO;
+                }
+            }
+        }
+    }
+    static class Rock implements GameItem{
+
+        @Override
+        public String eval(int computerChoice) {
+            {
+                {
+                    if (Objects.equals(computerChoice, R.id.btnPaper)) {
+                        return LOSES_TO;
+                    } else if (Objects.equals(computerChoice, R.id.btnRock)) {
+                        return TIES;
+                    } else {
+                        return BEATS;
+                    }
+                }
+            }
+        }
+    }
+    static class Scissors implements GameItem{
+
+        @Override
+        public String eval(int computerChoice) {
+            {
+                {
+                    if (Objects.equals(computerChoice, R.id.btnPaper)) {
+                        return BEATS;
+                    } else if (Objects.equals(computerChoice, R.id.btnRock)) {
+                        return LOSES_TO;
+                    } else {
+                        return TIES;
+                    }
+                }
+            }
+        }
+    }
+    public static String evaluateWinner(Integer playerSelectedChoice, final Integer computerSelectedChoice) {
         /*
          TODO: Create an Interface that has a super type method named eval.
          TODO: Create classes that implement the interface.
@@ -39,10 +91,27 @@ public class GameUtils {
          TODO: Implement the logic for each of the game types to determine the winner.
          */
 
+
+
         //logic goes here
 
-        return GameUtils.BEATS;
-    }
+        //Variable to hold the item the player selects
+        GameItem PlayerItem;
+
+        if (playerSelectedChoice == R.id.btnPaper) {
+            PlayerItem = new Paper();
+        }
+        else if (playerSelectedChoice == R.id.btnRock) {
+            PlayerItem = new Rock();
+        }
+        else{
+            PlayerItem = new Scissors();
+        }
+
+        return PlayerItem.eval(computerSelectedChoice);
+        }
+
+
 
     public static Integer convertButtonToImage(Integer buttonChoice) {
         if(buttonChoice==R.id.btnRock){
